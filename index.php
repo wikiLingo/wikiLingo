86e81b8e8e0c17072e0616d0a2095274fe482b98
Robert Plummer <robertleeplummerjr@gmail.com>
2013-06-25 13:58:33 -0400

    Add zend and index.php for testing

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'Zend/Loader/StandardAutoloader.php';
+$loader = new Zend\Loader\StandardAutoloader(array('autoregister_zf' => true));
+//$loader->registerNamespace('WikiLingo');
+//$wL = new WikiLingo();
+echo "test";
