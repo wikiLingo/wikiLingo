f4f0b666b72a6ca873389ac8d979783c521e0bb3
Lee Daniel Harris <dangerousleeharris@gmail.com>
2013-07-03 13:12:44 -0400

    Added "flash" to Plugin, linking to a very awesome video

--- index.php
+++ index.php
@@ -11,16 +11,8 @@ $loader
     ->registerNamespace('WikiLingoWYSIWYG', $dir)
 	->register();
 
-$original = "{DIV(color='purple' width='100px')}
-    {DIV()}
-        test
-    {DIV}
-{DIV}
-{DIV(color='purple' width='100px')}
-    {DIV()}
-        test
-    {DIV}
-{DIV}";
+$original = "{FLASH(movie='//www.youtube.com/embed/Plz-bhcHryc')}{FLASH}";
+
 
 $wikiLingo = new WikiLingo();
 $output = $wikiLingo->parse($original);
