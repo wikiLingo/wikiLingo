75727d4ea2b658e2ca3cdbc2d5e264e1e31b0533
Robert Plummer <robertleeplummerjr@gmail.com>
2013-10-24 22:20:39 -0400

    Remove Zend, don't need it.  Add auto loader, and testing framework.

---
+++ Tests/injected.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robert
+ * Date: 10/24/13
+ * Time: 6:11 PM
+ */ 
---
+++ autoload.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robert
+ * Date: 10/24/13
+ * Time: 5:07 PM
+ */ 
