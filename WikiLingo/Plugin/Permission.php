588b2665519c681df3b12542f65ba6b6da7b58a3
Robert Plummer <robertleeplummerjr@gmail.com>
2014-04-15 22:07:39 -0400

    Added permission plugin

---
+++ WikiLingo/Plugin/Permission.php
@@ -0,0 +1,44 @@
+<?php
+namespace WikiLingo\Plugin;
+
+use WikiLingo;
+
+/**
+ * Class Permission
+ * @package WikiLingo\Plugin
+ */
+class Permission extends Base
+{
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->htmlTagType = 'div';
+        $this->draggable = false;
+
+        $this->label = 'Permission';
+        $this->detailedAttributes['contenteditable'] = 'true';
+        $this->allowWhiteSpace = true;
+        $this->allowLines = true;
+        $this->permissible = false;
+    }
+
+    /**
+     * @param WikiLingo\Expression\Plugin $plugin
+     * @param string $body
+     * @param WikiLingo\Renderer $renderer
+     * @param WikiLingo\Parser $parser
+     * @return string
+     */
+    public function render(WikiLingo\Expression\Plugin &$plugin, &$body, &$renderer, &$parser)
+    {
+        if ($this->permissible) {
+            $result = parent::render($plugin, $body, $renderer, $parser);
+
+            return $result;
+        } else {
+            return '';
+        }
+    }
+}
